1a09470f60d6dc38577321ea38a477e0a93a4868
Andrew Mellen <andrew_mellen@icloud.com>
2025-12-05 16:55:58 -0500

    add new parser, fix slices test

--- 2025/utils/src/fileparse.zig
+++ 2025/utils/src/fileparse.zig
@@ -134,3 +134,88 @@ test "Can parse 2D grid" {
     try std.testing.expectEqualSlices(u8, &[_]u8{ 1, 2, 3, 4, 5 }, items[0].items);
     try std.testing.expectEqualSlices(u8, &[_]u8{ 5, 4, 3, 2, 1 }, items[1].items);
 }
+
+pub fn TwoSectionParser(
+    comptime T1: type,
+    comptime T2: type,
+    comptime handle_t1_line: fn (allocator: std.mem.Allocator, line: []const u8) anyerror!T1,
+    comptime handle_t2_line: fn (allocator: std.mem.Allocator, line: []const u8) anyerror!T2,
+) type {
+    return struct {
+        gpa: std.mem.Allocator,
+
+        pub fn init(gpa: std.mem.Allocator) @This() {
+            return .{ .gpa = gpa };
+        }
+
+        pub fn parse(self: @This(), file_name: []const u8) !struct { std.ArrayList(T1), std.ArrayList(T2) } {
+            const file = try std.fs.cwd().openFile(file_name, .{ .mode = .read_only });
+            defer file.close();
+
+            var buffer: [4096]u8 = undefined;
+            var reader = file.reader(&buffer);
+
+            var list1 = try std.ArrayList(T1).initCapacity(self.gpa, 1024);
+            errdefer list1.deinit(self.gpa);
+
+            var list2 = try std.ArrayList(T2).initCapacity(self.gpa, 1024);
+            errdefer list2.deinit(self.gpa);
+
+            var in_first_section: bool = true;
+
+            while (reader.interface.takeDelimiter('\n')) |opt_line| {
+                if (opt_line == null) {
+                    break;
+                }
+
+                const line = opt_line.?;
+
+                // skip empty lines
+                if (line.len == 0) {
+                    in_first_section = false;
+                    continue;
+                }
+
+                if (in_first_section) {
+                    const new_item = try handle_t1_line(self.gpa, line);
+
+                    try list1.append(self.gpa, new_item);
+                } else {
+                    const new_item = try handle_t2_line(self.gpa, line);
+                    try list2.append(self.gpa, new_item);
+                }
+            } else |err| if (err != error.EndOfStream) return err;
+
+            return .{ list1, list2 };
+        }
+    };
+}
+
+test "can parse multi-section file" {
+    const allocator = std.testing.allocator;
+
+    const Parser1 = struct {
+        fn parse(_: std.mem.Allocator, line: []const u8) !u64 {
+            return try std.fmt.parseInt(u64, line, 10);
+        }
+    };
+
+    const Parser2 = struct {
+        fn parse(gpa: std.mem.Allocator, line: []const u8) ![]const u8 {
+            return gpa.dupe(u8, line);
+        }
+    };
+
+    const parser = TwoSectionParser(u64, []const u8, Parser1.parse, Parser2.parse).init(allocator);
+    var actual = try parser.parse("test_data/two_section.txt");
+    defer actual.@"0".deinit(allocator);
+    defer {
+        for (actual.@"1".items) |s| {
+            allocator.free(s);
+        }
+        actual.@"1".deinit(allocator);
+    }
+
+    try std.testing.expectEqualSlices(u64, &[_]u64{ 1, 2, 3, 4, 5 }, actual.@"0".items);
+    try std.testing.expectEqualDeep(&[_][]const u8{ "abcd", "efgh" }, actual.@"1".items);
+}
--- 2025/utils/src/slices.zig
+++ 2025/utils/src/slices.zig
@@ -13,22 +13,26 @@ pub fn two_dimensional_arraylists_to_slices(gpa: std.mem.Allocator, comptime T: 
 }
 
 test "arraylists to slices works" {
-    const allocator = std.heap.page_allocator;
+    const allocator = std.testing.allocator;
 
     var grid = std.ArrayList(std.ArrayList(u8)).empty;
-    defer grid.deinit(allocator);
+    errdefer grid.deinit(allocator);
 
     var row1 = std.ArrayList(u8).empty;
     try row1.appendSlice(allocator, &[_]u8{ 1, 2, 3, 4, 5 });
-    defer row1.deinit(allocator);
     var row2 = std.ArrayList(u8).empty;
     try row2.appendSlice(allocator, &[_]u8{ 5, 4, 3, 2, 1 });
-    defer row2.deinit(allocator);
 
     try grid.append(allocator, row1);
     try grid.append(allocator, row2);
 
     const actual = try two_dimensional_arraylists_to_slices(allocator, u8, &grid);
+    defer {
+        for (actual) |row| {
+            allocator.free(row);
+        }
+        allocator.free(actual);
+    }
 
     const expected: []const []const u8 = &.{
         &.{ 1, 2, 3, 4, 5 },
